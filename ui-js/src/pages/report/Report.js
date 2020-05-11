77621b49fe55fe74ef94070dfb268b7ebb4b8205
Bart Cant <bart.cant@gmail.com>
2020-05-11 12:40:21 -0400

    Updates to UX

--- ui-js/src/pages/report/Report.js
+++ ui-js/src/pages/report/Report.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Contracts from "../../components/Contracts/Contracts";
-import { useStreamQuery, useLedger } from "@daml/react";
+import { useStreamQuery, useLedger, useParty } from "@daml/react";
 import { Main } from "@daml2js/Covid19-0.0.1/";
 
 
@@ -8,25 +8,29 @@ import { Main } from "@daml2js/Covid19-0.0.1/";
 
 export default function Report() {
 
+  const operator = useParty();
   const ledger = useLedger();
   const assets = useStreamQuery (Main.Network);
-  const exerciseInviteHealthClinic = function(cid, healthclinic ) {
-    ledger.exercise(Main.Network.InviteHealthClinic, cid, { healthclinic })}; 
-  const exerciseInviteCitizen = function(cid , citizen ) {
-     ledger.exercise(Main.Network.InviteHealthCitizen, cid, { citizen })};
 
+  const exerciseInviteHealthClinic = function(cid, healthclinic ) {
+    ledger.exercise(Main.Network.InviteHealthClinic, cid, { operator, healthclinic } ); };
+  const exerciseInviteCitizen = function(cid, citizen ) {
+    ledger.exercise(Main.Network.InviteCitizen, cid, { operator, citizen } ); };
+ 
   return (
     <>
       <Contracts
         contracts={assets.contracts}
         columns={[
+          ["ContractId", "contractId"],
           ["TemplateId", "templateId"],
           ["Operator", "payload.operator"]
+        
         ]}
          
        actions={[
-         ["InviteHealthClinic", (c, healthclinic) => { exerciseInviteHealthClinic(c.contractId, healthclinic); }, "Healthclinic"],
-         ["InviteCitizen", (c, citizen) => { exerciseInviteCitizen(c.contractId, citizen); }, "Citizen"]
+         ["InviteHealthClinic", (c, healthclinic ) => { exerciseInviteHealthClinic(c.contractId, healthclinic); }, "Healthclinic"],
+         ["InviteCitizen", (c, citizen ) => { exerciseInviteCitizen(c.contractId, citizen); }, "Citizen"]
         ]}
       />
     </>

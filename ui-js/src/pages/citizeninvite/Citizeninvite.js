a38813313e5911eb441a4390f0af9bab2efa2cca
Bart Cant <bart.cant@gmail.com>
2020-06-02 08:52:00 -0400

    Update DAML and  Invite screens with new forms

--- ui-js/src/pages/citizeninvite/Citizeninvite.js
+++ ui-js/src/pages/citizeninvite/Citizeninvite.js
@@ -3,41 +3,181 @@ import React from "react";
 import Contracts from "../../components/Contracts/Contracts";
 import { useStreamQuery, useParty, useLedger } from "@daml/react";
 import { Main } from "@daml2js/Covid19-0.0.1/";
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 export default function Clinicinvite() {
 
+  
   const citizen = useParty();
   const operator = "Operator"; 
-  const healthstate = "NCHealth";
   const ledger = useLedger();
   const assets = useStreamQuery (Main.CitizenInvitation);
+
+
+  const [conductModalOpen, setConductModalOpen] = React.useState(false);
+  const [curContractId, setContractId] = React.useState('');
+  const [citizendetails, setConductForm] = React.useState({
+    idtype: '',
+    ssn: '',
+    did: '',
+    firstname: '',
+    lastname: '',
+    email:'',
+    accept_vcoremail: '',
+    Hippa_accept: '',
+    insurance_id: ''
+  });
+
+  const [checked, setChecked] = React.useState(true);
+
+  const handleConductModalOpen = (cid = '') => {
+    setContractId(cid);
+    setConductModalOpen(true);
+  };
+
+  const handleConductModalClose = () => {
+    setConductModalOpen(false);
+  };
+
+  const handleConductChange = (idx, event) => {
+    setConductForm({
+      ...citizensdetails,
+      [idx]: event.target.value
+    })
+  }
+
+ 
+
 
   const exerciseCitizenAccept = function(cid ) {
     console.log("operator :" + operator);
     console.log("citizen : " + citizen);
-    console.log("cid: "+ cid);
-    ledger.exercise(Main.CitizenInvitation.AcceptCitizenInvitation, cid, { operator, citizen } ); };
+    console.log("cid: " + curContractId);
     
-    const exerciseCitizenVC = function(cid ) {
-      console.log("operator :" + operator);
-      console.log("citizen : " + citizen);
-      console.log("cid: "+ cid);
-      ledger.exercise(Main.CitizenInvitation.AcceptCitizenVC, cid, { operator, healthstate, citizen } ); };
-  return (
+
+    ledger.exercise(Main.CitizenInvitation.AcceptCitizenInvitation, curContractId, { operator, citizen, citizendetails } ); };
+    
+  
+return (
     <>
       <Contracts
         contracts={assets.contracts}
-        columns={[
-          ["ContractId", "contractId"],
-          ["Operator", "payload.operator"],
-          ["Citizen", "payload.citizen"]
+        actions={[
+         ["Accept Invitation", (c) => { handleConductModalOpen(c.contractId);}], 
+
         ]}
-         
-       actions={[
-         ["Accept Invitation", (c) => { exerciseCitizenAccept(c.contractId); }],  
-         ["Accept VC",(c) => { exerciseCitizenVC(c.contrcatId);}]
-       ]}
-      />
-    </>
-  );
-}
+      
+        />
+      
+       <Dialog
+          open={conductModalOpen}
+          onClose={handleConductModalClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          >
+          <DialogTitle id="alert-dialog-title">{"CONDUCT TEST"}</DialogTitle>
+          <DialogContent>
+            <div>
+              <TextField
+                label="ID Type"
+                placeholder="ID Type"
+                value={citizendetails.idtype}
+                onChange={(e) => handleConductChange('idtype', e)}
+              />
+            </div>
+
+            <div>
+              <TextField
+                label="SSN"
+                placeholder="SSN"
+                value={citizendetails.ssn}
+                onChange={(e) => handleConductChange('ssn', e)}
+              />
+            </div>
+      
+            <div>
+              <TextField
+                label="DID"
+                placeholder="DID"
+                value={citizendetails.did}
+                onChange={(e) => handleConductChange('did', e)}
+              />
+            </div>
+
+            <div>
+              <TextField
+                label="lastname"
+                placeholder="lastname"
+                value={citizendetails.lastname}
+                onChange={(e) => handleConductChange('lastname', e)}
+              />
+            </div>
+            <div>
+              <TextField
+                label="firstname"
+                placeholder="firstname"
+                value={citizendetails.did}
+                onChange={(e) => handleConductChange('firstname', e)}
+              />
+            </div>
+            <div>
+              <TextField
+                label="email"
+                placeholder="email"
+                value={citizendetails.email}
+                onChange={(e) => handleConductChange('email', e)}
+              />
+            </div>
+             <div>
+               <FormControl component="Accept Credentials">
+                 <FormLabel component="legend">Accept Credentials</FormLabel>
+                    <RadioGroup aria-label="gender" name="gender1" value={citizendetails.accept_vcoremail} onChange={(e) => handleConductChange('accept_vcoremail', e)}>
+                      <FormControlLabel value="email" control={<Radio />} label="Email" />
+                      <FormControlLabel value="VC" control={<Radio />} label="Verifiable Credential" />
+                    </RadioGroup>
+                </FormControl>
+              <RadioButton
+              />
+            </div>
+
+            <div>
+            <FormControlLabel
+              value="start"
+                control={<Checkbox
+                  checked={checked}
+                  value={citizendetails.hippa_accept}
+                  onChange={(e) => handleConductChange('hippa_accept', e)}
+                  label="Accept HIPPA"
+             />}
+             />
+           </div>
+
+           <div>
+              <TextField
+                label="insuranceid"
+                placeholder="Insurance id"
+                value={citizendetails.insurance_id}
+                onChange={(e) => handleConductChange('insurance_id', e)}
+              />
+            </div>
+
+
+
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleConductModalClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={() => exerciseCitizenAccept()} color="primary" autoFocus>
+              Test
+            </Button>
+          </DialogActions>
+          </Dialog>
+          </>
+
+          );
+        }
--- ui-js/src/pages/clinicinvite/Clinicinvite.js
+++ ui-js/src/pages/clinicinvite/Clinicinvite.js
@@ -11,12 +11,43 @@ export default function Clinicinvite() {
   const ledger = useLedger();
   const assets = useStreamQuery (Main.HealthClinicInvitation);
 
+  const [conductModalOpen, setConductModalOpen] = React.useState(false);
+  const [curContractId, setContractId] = React.useState('');
+  const [healthclinicdetails, setConductForm] = React.useState({
+    id: '',
+    did: '',
+    hcname: '',
+    hcaddress: '',
+    hctel:'',
+    hccontactname: '',
+    hccontactemail: '',
+    hccontacttel: ''
+  });
+
+
+  const handleConductModalOpen = (cid = '') => {
+    setContractId(cid);
+    setConductModalOpen(true);
+  };
+
+  const handleConductModalClose = () => {
+    setConductModalOpen(false);
+  };
+
+  const handleConductChange = (idx, event) => {
+    setConductForm({
+      ...citizensdetails,
+      [idx]: event.target.value
+    })
+  }
+
+
+
   const exerciseHealthClinicAccept = function(cid ) {
     console.log("operator :" + operator);
     console.log("healthclinic : " + healthclinic);
-    console.log("cid: "+ cid);
-    ledger.exercise(Main.HealthClinicInvitation.AcceptInvitation, cid, { operator, healthclinic } ); };
-    
+    console.log("cid: "+ curContractId);
+    ledger.exercise(Main.HealthClinicInvitation.AcceptInvitation, curContractId, { operator, healthclinic, healthclinicdetails } ); };
     
   return (
     <>
@@ -29,9 +60,98 @@ export default function Clinicinvite() {
         ]}
          
        actions={[
-         ["Accept Invitation", (c) => { exerciseHealthClinicAccept(c.contractId); }]  
+         ["Accept Invitation", (c) => { handleConductModalOpen(c.contractId); }]  
        ]}
       />
-    </>
-  );
-}
+
+<Dialog
+          open={conductModalOpen}
+          onClose={handleConductModalClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          >
+          <DialogTitle id="alert-dialog-title">{"CONDUCT TEST"}</DialogTitle>
+          <DialogContent>
+            <div>
+              <TextField
+                label="ID"
+                placeholder="ID"
+                value={healthclinicdetails.id}
+                onChange={(e) => handleConductChange('id', e)}
+              />
+            </div>
+      
+            <div>
+              <TextField
+                label="DID"
+                placeholder="DID"
+                value={healthclinicdetails.did}
+                onChange={(e) => handleConductChange('did', e)}
+              />
+            </div>
+
+            <div>
+              <TextField
+                label="Health Clinic Name"
+                placeholder="hcname"
+                value={healthclinicdetails.hcname}
+                onChange={(e) => handleConductChange('hcname', e)}
+              />
+            </div>
+            <div>
+              <TextField
+                label="Health Clinic Address "
+                placeholder="hcaddress"
+                value={healthclinicdetails.hcaddress}
+                onChange={(e) => handleConductChange('hcaddress', e)}
+              />
+            </div>
+            <div>
+              <TextField
+                label="Health Clinic Telephone"
+                placeholder="hctel"
+                value={healthclinicdetails.hctel}
+                onChange={(e) => handleConductChange('hctel', e)}
+              />
+            </div>
+           <div>
+              <TextField
+                label="Contact Name"
+                placeholder="hccontactname"
+                value={healthclinicdetails.hccontactname}
+                onChange={(e) => handleConductChange('hccontactname', e)}
+              />
+            </div>
+            <div>
+              <TextField
+                label="Contact Email"
+                placeholder="hccontactemail"
+                value={healthclinicdetails.hccontactemail}
+                onChange={(e) => handleConductChange('hccontactemail', e)}
+              />
+            </div>
+            <div>
+              <TextField
+                label="Contact Telephone"
+                placeholder="hccontacttel"
+                value={healthclinicdetails.hccontacttel}
+                onChange={(e) => handleConductChange('hccontacttel', e)}
+              />
+            </div>
+
+
+
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleConductModalClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={() => exerciseCitizenAccept()} color="primary" autoFocus>
+              Test
+            </Button>
+          </DialogActions>
+          </Dialog>
+          </>
+
+          );
+        }

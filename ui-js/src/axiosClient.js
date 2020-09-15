138ec92e3505d0e14c19ca8e32ac6064b8dcfe11
star123good <star921str@hotmail.com>
2020-09-16 01:22:34 +0800

    deploy projectdabl

--- ui-js/src/axiosClient.js
+++ ui-js/src/axiosClient.js
@@ -1,13 +1,13 @@
 var axios = require('axios');
 
-var axiosClient = axios.create({
-  baseURL: 'http://localhost:3002/'
-});
+// var axiosClient = axios.create({
+//   baseURL: 'http://localhost:3002/'
+// });
 
 // AWS Node.JS server
-// var axiosClient = axios.create({
-//  baseURL: 'https://daml-covid19.vcredserver.com/'
-//});
+var axiosClient = axios.create({
+ baseURL: 'https://daml-covid19.vcredserver.com/'
+});
 
 
 module.exports = axiosClient;

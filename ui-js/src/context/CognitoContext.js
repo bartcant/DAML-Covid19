4d1e9b27f6ab3f27d39d9b96c29b189b1efc903e
star123good <star921str@hotmail.com>
2020-09-18 00:31:51 +0800

    update Cognito Contex js

--- ui-js/src/context/CognitoContext.js
+++ ui-js/src/context/CognitoContext.js
@@ -2,7 +2,7 @@
 import {
   AuthenticationDetails,
   CognitoUserPool,
-//   CognitoUserAttribute,
+  CognitoUserAttribute,
   CognitoUser,
 //   CognitoUserSession,
 } from "amazon-cognito-identity-js";
@@ -39,6 +39,13 @@ async function cognitoLogIn(username, password) {
                     console.log("[cognitoLogIn] failed", err.message);
                     resolve(null);
                 },
+                mfaRequired: function(codeDeliveryDetails) {
+                    console.log("[cognitoLogIn] mfaRequired", codeDeliveryDetails);
+                    // MFA is required to complete user authentication.
+                    // Get the code from user and call
+                    // cognitoUser.sendMFACode(mfaCode, this);
+                    resolve(codeDeliveryDetails);
+                },
                 newPasswordRequired: function(userAttributes) {
                     console.log("[cognitoLogIn] newPasswordRequired", userAttributes);
                     resolve(userAttributes);
@@ -52,18 +59,32 @@ async function cognitoLogIn(username, password) {
 }
 
 
+function cognitoSignUp(username, callback) {
+    // default password
+    const password = '1qazXSW@';
 
-async function cognitoSignUp(username, password, callback) {
     const userPool = new CognitoUserPool(POOL_DATA);
 
     var attributeList = [];
+    var dataEmail = {
+        Name: 'email',
+        Value: 'test@email.com',
+    };
+    // var dataPhoneNumber = {
+    //     Name: 'phone_number',
+    //     Value: '+15555555555',
+    // };
+    var attributeEmail = new CognitoUserAttribute(dataEmail);
+    // var attributePhoneNumber = new CognitoUserAttribute(dataPhoneNumber);
+    attributeList.push(attributeEmail);
+    // attributeList.push(attributePhoneNumber);
 
     userPool.signUp(username, password, attributeList, null, function(
         err,
         result
     ) {
         if (err) {
-            alert(err.message || JSON.stringify(err));
+            console.log(err.message || JSON.stringify(err));
             return callback(null);
         }
         var cognitoUser = result.user;
@@ -73,4 +94,22 @@ async function cognitoSignUp(username, password, callback) {
 }
 
 
-export { cognitoLogIn, cognitoSignUp };
+function changePassword(username, oldPassword, newPassword, callback) {
+    const userPool = new CognitoUserPool(POOL_DATA);
+
+    var userData = {
+        Username: username,
+        Pool: userPool,
+    };
+    var cognitoUser = new CognitoUser(userData);
+
+    cognitoUser.changePassword(oldPassword, newPassword, function(err, result) {
+        if (err) {
+            console.log(err.message || JSON.stringify(err));
+            return;
+        }
+        console.log('[changePassword]' + result);
+    });
+}
+
+export { cognitoLogIn, cognitoSignUp, changePassword };

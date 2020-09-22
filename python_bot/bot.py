9ce180b923aca017b54dc20843e4a8bb70a49259
star123good <star921str@hotmail.com>
2020-09-23 04:33:09 +0800

    update python bot

--- python_bot/bot.py
+++ python_bot/bot.py
@@ -19,31 +19,32 @@ def main():
     network.set_config(url=url)
     client = network.aio_party(party)
 
-    contractIdProposals = []
 
     @client.ledger_ready()
     async def bot_ready(event): 
         logging.info(f'user_bot for party {client} is ready')
         contracts = await event.acs_find_nonempty('Main.Covid19Test')
         logging.info(contracts)
-        # dazl.exercise(contracts[0].cid, 'UpdateCovid19Test', {
-        #     'newcovid19TestData' : contracts[0]['covid19testdata']
-        # })
+        
 
     @client.ledger_created('Main.Covid19Test')
     def on_message(event):
-        contractIdProposals.append(event.cid)
         logging.info("[on_message]")
+        logging.info(event.cid)
         logging.info(event.cdata)
-        logging.info(contractIdProposals)
+        newData = event.cdata['covid19testdata']
+        newData['testresult'] = 'Negative'
+        logging.info(newData)
+        return dazl.exercise(event.cid, 'UpdateCovid19Test', {
+            'newcovid19TestData' : newData,
+            })
 
     @client.ledger_exercised('Main.Covid19Test','UpdateCovid19Test')
-    def final_agreement_created(event):
-        contractIdProposals.remove(event.cid)
-        logging.info("[final_agreement_created]")
-        logging.info(contractIdProposals)
-        for contractId in contractIdProposals:
-            client.submit_exercise(contractId, 'RejectOffer')
+    def on_updated(event):
+        logging.info("[on_updated]")
+        logging.info(event.cid)
+        logging.info(event.cdata)
+
 
     network.run_forever()
 

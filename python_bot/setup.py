f5f774dbe11d71d362093c18b260622275e0789c
star123good <star921str@hotmail.com>
2020-09-24 23:10:38 +0800

    update python bot for poetry

--- python_bot/setup.py
+++ python_bot/setup.py
@@ -4,7 +4,6 @@ setup(name='bot',
       version='1.0.0',
       description='Covid19 DAML Python Bot',
       author='STAR',
-      license='Apache2',
       install_requires=['dazl'],
       packages=['python_bot'],
       include_package_data=True)

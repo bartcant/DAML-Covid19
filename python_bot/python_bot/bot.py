0872707e2ebccdf81c09e8afd6070124824c9bf9
star123good <star921str@hotmail.com>
2020-09-30 21:54:29 +0800

    fix async loop in onready of python bot

--- python_bot/python_bot/bot.py
+++ python_bot/python_bot/bot.py
@@ -3,6 +3,7 @@ import uuid
 import datetime
 import time
 import dazl
+import asyncio
 
 dazl.setup_default_logger(logging.INFO)
 EPOCH = datetime.datetime.utcfromtimestamp(0)
@@ -20,30 +21,39 @@ def main():
     client = network.aio_party(party)
 
 
+    async def callUpdateCovid19Test(cid, newData):
+        logging.info("[callUpdateCovid19Test]")
+        if newData['testresult'] == 'Pending':
+            newData['testresult'] = 'Negative'
+            logging.info(cid)
+            logging.info(newData)
+            # using UpdateCovid19Test choice
+            return dazl.exercise(cid, 'UpdateCovid19Test', {
+                'newcovid19TestData' : newData,
+                })
+
+
     @client.ledger_ready()
     async def bot_ready(event): 
         logging.info(f'user_bot for party {client} is ready')
         contracts = await event.acs_find_nonempty('Main.Covid19Test')
         logging.info(contracts)
-        # return dazl.exercise(contracts[0], 'UpdateCovid19Test', {})
+
+        tasks = [
+            callUpdateCovid19Test(contractID, contractData['covid19testdata']) 
+            for contractID, contractData in contracts.items()
+            ]
+        for f in asyncio.as_completed(tasks):
+            result = await f
+        return result
         
 
     @client.ledger_created('Main.Covid19Test')
     def on_message(event):
         logging.info("[on_message]")
-        logging.info(event.cid)
-        logging.info(event.cdata)
-        newData = event.cdata['covid19testdata']
-        newData['testresult'] = 'Negative'
-        logging.info(newData)
-        # 1st option
-        # using UpdateCovid19Test choice
-        return dazl.exercise(event.cid, 'UpdateCovid19Test', {
-            'newcovid19TestData' : newData,
-            })
-        # 2nd option
-        # using UpdateTestResult choice
-        # return dazl.exercise(event.cid, 'UpdateTestResult', {})
+        # logging.info(event.cid)
+        # logging.info(event.cdata)
+        return callUpdateCovid19Test(event.cid, event.cdata['covid19testdata'])
 
     @client.ledger_exercised('Main.Covid19Test','UpdateCovid19Test')
     def on_updated(event):
@@ -51,14 +61,10 @@ def main():
         logging.info(event.cid)
         logging.info(event.cdata)
 
-    @client.ledger_exercised('Main.Covid19Test','UpdateTestResult')
-    def on_updated_result(event):
-        logging.info("[on_updated_result]")
-        logging.info(event.cid)
-        logging.info(event.cdata)
-
 
     network.run_forever()
+
+
 
 if __name__ == '__main__':
     main()

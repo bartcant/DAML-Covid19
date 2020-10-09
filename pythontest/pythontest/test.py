413cf2c04f8c3981bd25edfb9b67389dfdbf440f
star123good <star921str@hotmail.com>
2020-10-09 23:54:31 +0800

    modify test.py

--- pythontest/pythontest/test.py
+++ pythontest/pythontest/test.py
@@ -12,20 +12,29 @@ EPOCH = datetime.datetime.utcfromtimestamp(0)
 def main():
 
     # url = os.getenv('DAML_LEDGER_URL')
-    # url = 'http://localhost:6865'
+    url = 'http://localhost:6865'
     # party = os.getenv('DAML_LEDGER_PARTY')
     party = 'Operator'
 
     network = dazl.Network()
 
-    # network.set_config(url=url)
+    network.set_config(url=url)
     client = network.aio_party(party)
 
-    with dazl.simple_client('http://localhost:6865', 'Operator') as client:
+    @client.ledger_ready()
+    async def bot_ready(event): 
+        logging.info(f'user_bot for party {client} is ready')
         contract = {'operator': 'Operator',
                     'party': 'Citi', 'roletype': 'Citizen'}
-        client.ready()
-        client.submit_create('Main.PartyInvitation', contract)
+        return client.submit_create('Main.PartyInvitation', contract)
+
+    # with dazl.simple_client('http://localhost:6865', 'Operator') as client:
+    #     contract = {'operator': 'Operator',
+    #                 'party': 'Citi', 'roletype': 'Citizen'}
+    #     client.ready()
+    #     client.submit_create('Main.PartyInvitation', contract)
+
+    network.run_forever()
 
 
 if __name__ == '__main__':
